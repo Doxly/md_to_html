40a8ed74efb6cce0cdfa82873375893bf71a8098
Doxly <pva33@mail.ru>
2015-05-07 16:54:14 +0400

    new structure of project

---
+++ tests/md_to_html_tests.py
@@ -0,0 +1,15 @@
+from nose.tools import *
+
+import md_to_html
+
+class md_to_html_tests(object):
+    """docstring for NAME_tests"""
+        
+    def setup(self):
+        print "SETUP!"
+
+    def teardown(self):
+        print "TEAR DOWN!"
+
+    def test_basic(self):
+        print "I RAN!"
---
+++ tests/test.cmd
@@ -0,0 +1,5 @@
+rem проверка скрипта md_to_html.py
+
+mdtohtml.py -css test.md
+
+test.html
